9b4dfa2fb72b1dd2ed9cf2e1f175f7cbef13ad67
Frank Strocco <frank.strocco@gmail.com>
2019-10-27 13:19:03 -0700

    created databases, tables and user with privilages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :locations
+  resources :technicians
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
